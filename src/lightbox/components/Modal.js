be4c0e444fe8371de043c363629bfc75e8e800b4
Kopy_TKG <47574331+KopyTKG@users.noreply.github.com>
2021-05-11 13:05:54 +0200

    Stable update - Lightbox working for single img

---
+++ src/lightbox/components/Modal.js
@@ -0,0 +1,30 @@
+import {motion} from 'framer-motion';
+
+const Modal = ({ImageSrc, setImageSrc}) => {
+    // Close event
+    const handleClick = (e) => {
+        // Background need to have backdrop class
+        if(e.target.classList.contains("backdrop")) 
+            // reset imgSrc
+            setImageSrc(null);
+    }
+
+    return(
+        <motion.div 
+            className="backdrop background" 
+            onClick={handleClick}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+        >
+            <motion.img
+                src={ImageSrc}
+                alt="lightboxImg"
+                initial={{y: "-100vh"}}
+                animate={{y: 0}}
+                
+            />
+        </motion.div>
+    );
+}
+
+export default Modal;
